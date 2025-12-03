a4876a53fcd63ad46038ee76e4a9aed52e414af5
dahoon hong <slayerboxr@gmail.com>
2025-12-03 22:00:24 +0900

    fix position

--- Build/publish.loader.js
+++ Build/publish.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 54228469,
+    wasmFileSize: 54228389,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
